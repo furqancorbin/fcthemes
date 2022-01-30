f17b2982e7240e832a9eca7d0b98d494e1b82a8e
Furqancorbin <60383939+furqancorbin@users.noreply.github.com>
2022-01-30 11:02:37 +0500

    Create external script js test.js

---
+++ external script js test.js
@@ -0,0 +1,9 @@
+var myurl = "www.example.com";
+ var currenturl = window.location.hostname;
+    if(myurl != currenturl) {
+     function pageRedirect() {
+       alert ("External Working Success"); 
+    }      
+    setTimeout("pageRedirect()", 1000);  
+    }
+
