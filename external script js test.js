ff315a76ec843542d2ce9ba6cee2d2734078be81
Furqancorbin <60383939+furqancorbin@users.noreply.github.com>
2022-06-24 22:59:44 +0500

    Delete external script js test.js

--- external script js test.js
+++
@@ -1,8 +0,0 @@
-var myurl = "www.example.com";
- var currenturl = window.location.hostname;
-    if(myurl != currenturl) {
-     function pageRedirect() {
-       alert ("External Working Success"); 
-    }      
-   setTimeout("pageRedirect()", 1);  
-    }
